57ea8439676e8fa0ecf614b19f2e0935e15be629
Eternal254 <137845281+Eternal254@users.noreply.github.com>
2025-01-28 01:43:59 -0600

    Deslinde

--- sire/gob/views/__init__.py
+++ sire/gob/views/__init__.py
@@ -1,5 +1,5 @@
 from .cliente import ClienteViewSet
 from .empresa import EmpresaViewSet
 from .factura import FacturaElectronicaViewSet
-from .login import login_view, logout_view, aviso_privacidad_view
+from .login import login_view, logout_view, aviso_privacidad_view, deslinde_responsabilidad_view
 from .custom_login import custom_login
