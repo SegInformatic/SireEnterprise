2874837d5e40ea7ed1cfcf1db61b1d5d25bf7efd
Eternal254 <137845281+Eternal254@users.noreply.github.com>
2025-01-28 01:12:27 -0600

    Aviso de privacidad nice y login

--- sire/gob/views/login.py
+++ sire/gob/views/login.py
@@ -13,7 +13,7 @@ def login_view(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('/')  # Cambia a la página que desees redirigir después de iniciar sesión
+                return redirect('/gob/empresa/')
             else:
                 messages.error(request, 'Usuario o contraseña incorrectos')
     return render(request, 'gob/login.html')
